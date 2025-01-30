834f7b2c8fe97ed4889c6d58cf6424a287641f3f
Johnny Oshika <johnnyoshika@gmail.com>
2025-01-30 07:52:58 -0800

    Add 2 column layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
-const App = () => {
-  return <div></div>;
-};
+function App() {
+  return (
+    <div style={{ display: 'flex', minHeight: '100vh' }}>
+      <div
+        style={{
+          flex: '1 1 20%',
+          borderRight: '1px solid #ddd',
+          padding: '1rem',
+        }}
+      ></div>
+      <div style={{ flex: '1 1 80%', padding: '20px' }}></div>
+    </div>
+  );
+}
 
 export default App;
