45c17d4bd902984300142240e0b932d77c6db1af
Johnny Oshika <johnnyoshika@gmail.com>
2025-01-30 08:57:54 -0800

    Add debug document trees

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { Block } from '@blocknote/core';
 import '@blocknote/core/fonts/inter.css';
 import { BlockNoteView } from '@blocknote/mantine';
 import '@blocknote/mantine/style.css';
 import { useCreateBlockNote } from '@blocknote/react';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [blocks, setBlocks] = useState<Block[]>([]);
+  const [html, setHTML] = useState<string>('');
+
   const editor = useCreateBlockNote();
+
+  const onChange = async () => {
+    setBlocks(editor.document);
+    setHTML(await editor.blocksToHTMLLossy(editor.document));
+  };
+
+  useEffect(() => {
+    // on mount, trigger initial conversion of the initial content to blocks and html
+    onChange();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div style={{ display: 'flex', minHeight: '100vh' }}>
       <div
@@ -15,7 +32,25 @@ function App() {
         }}
       ></div>
       <div style={{ flex: '1 1 80%', padding: '20px' }}>
-        <BlockNoteView editor={editor} theme="light" />
+        <div style={{ minHeight: '400px' }}>
+          <BlockNoteView
+            editor={editor}
+            theme="light"
+            onChange={onChange}
+          />
+        </div>
+        <h3>Document JSON:</h3>
+        <div className="document-tree">
+          <pre>
+            <code>{JSON.stringify(blocks, null, 2)}</code>
+          </pre>
+        </div>
+        <h3>Output HTML:</h3>
+        <div className="document-tree">
+          <pre>
+            <code>{html}</code>
+          </pre>
+        </div>
       </div>
     </div>
   );
